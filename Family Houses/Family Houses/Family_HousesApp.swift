d83e4db1565aab86c2b043107b910914f210e64f
Yumi Izumi <mail@yumi.to>
2026-02-09 14:42:07 +0900

    feat: create empty iOS project

---
+++ Family Houses/Family Houses/Family_HousesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Family_HousesApp.swift
+//  Family Houses
+//
+//  Created by yumi on 2026/02/09.
+//
+
+import SwiftUI
+
+@main
+struct Family_HousesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
